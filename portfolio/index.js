f54c71f33a79c1d17ff9beac88f00bfa7f7c8eca
tgriffabenitez <tgriffabenitez@frba.utn.edu.ar>
2023-01-02 16:17:17 -0300

    Navbar terminado

---
+++ portfolio/index.js
@@ -0,0 +1,7 @@
+/* Navbar toggle */
+const burgerIcon = document.querySelector("#burger");
+const navbarMenu = document.querySelector("#nav-links");
+
+burgerIcon.addEventListener("click", () => {
+    navbarMenu.classList.toggle("is-active");
+});
