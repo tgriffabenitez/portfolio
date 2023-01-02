f4e3595e7b3b0f01b715a64a81127fb2145c5c18
tgriffabenitez <tgriffabenitez@frba.utn.edu.ar>
2023-01-02 16:25:37 -0300

    home terminado

--- portfolio/index.js
+++ portfolio/index.js
@@ -5,3 +5,21 @@ const navbarMenu = document.querySelector("#nav-links");
 burgerIcon.addEventListener("click", () => {
     navbarMenu.classList.toggle("is-active");
 });
+
+
+/* Vanta wave animation */
+window.addEventListener('DOMContentLoaded', () => {
+    VANTA.WAVES({
+    el: "#vanta",
+    mouseControls: false,
+    touchControls: false,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    shininess: 0.00,
+    waveHeight: 30.00,
+    zoom: 0.65
+    })
+});
